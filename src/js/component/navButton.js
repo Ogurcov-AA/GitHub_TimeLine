4b290650cbc88a1f7b8c9c9b143416cbb4e69de5
Sasha Ogurtsov <sanekLLIypa@yandex.ru>
2021-07-14 16:40:00 +0300

    markup: add left menu with user info(img + name)

---
+++ src/js/component/navButton.js
@@ -0,0 +1,7 @@
+export default async function navButton(img, text){
+    let html = `<div class="rectangle-button">
+                    <img src="${img}" alt="">
+                    <h4>${text}</h4>
+                </div>`;
+    return html;
+}
