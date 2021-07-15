2b6cdcfc7d5f3d058cfd20c89d7175400d85281d
Sasha Ogurtsov <sanekLLIypa@yandex.ru>
2021-07-15 15:11:58 +0300

    feat: add save user info in session storage

--- src/js/store/userObject.js
+++ src/js/store/userObject.js
@@ -2,14 +2,15 @@ import axios from "axios";
 
 const userObject = {
     state: {
-        userName: null,
-        userAvatarUrl: null,
-        userPublicRepos: null,
-        userCreated: null,
+        userName: (JSON.parse(sessionStorage.getItem('userObject')))?.userName || null,
+        userAvatarUrl: (JSON.parse(sessionStorage.getItem('userObject')))?.userAvatarUrl || null,
+        userPublicRepos: (JSON.parse(sessionStorage.getItem('userObject')))?.userPublicRepos || null,
+        userCreated: (JSON.parse(sessionStorage.getItem('userObject')))?.userCreated || null,
     },
     mutations: {
         setInfo(state, data) {
             console.log(data)
+
             state.userName = data.userName
             state.userAvatarUrl = data.userAvatarUrl
             state.userPublicRepos = data.userPublicRepos
@@ -21,16 +22,24 @@ const userObject = {
             return new Promise((resolve, reject) => {
                 axios.get(`https://api.github.com/users/${userName}`)
                     .then(response => {
-                            window.store.userObject.mutations.setInfo(window.store.userObject.state,
-                                {
-                                    userName: response.data.login,
-                                    userAvatarUrl: response.data.avatar_url,
-                                    userPublicRepos: response.data.public_repos,
-                                    userCreated: response.data.created_at.slice(0, 10)
-                                })
+                        window.store.userObject.mutations.setInfo(window.store.userObject.state,
+                            {
+                                userName: response.data.login,
+                                userAvatarUrl: response.data.avatar_url,
+                                userPublicRepos: response.data.public_repos,
+                                userCreated: response.data.created_at.slice(0, 10)
+                            })
+                        if (sessionStorage.getItem('userObject'))
+                            sessionStorage.removeItem('userObject')
+                        sessionStorage.setItem('userObject', JSON.stringify({
+                            userName: response.data.login,
+                            userAvatarUrl: response.data.avatar_url,
+                            userPublicRepos: response.data.public_repos,
+                            userCreated: response.data.created_at.slice(0, 10)
+                        }))
                         resolve()
                     })
-                    .catch(rej=>{
+                    .catch(rej => {
                         reject('not found')
                     })
             })
