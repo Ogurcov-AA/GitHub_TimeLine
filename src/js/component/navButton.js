ca640dc6c5bb61ee220caac71705d05c25279569
Sasha Ogurtsov <sanekLLIypa@yandex.ru>
2021-07-21 19:56:27 +0300

    feat: add it is possible to interact with buttons

--- src/js/component/navButton.js
+++ src/js/component/navButton.js
@@ -1,7 +1,14 @@
-export default function navButton(img, text){
-    let html = `<div class="rectangle-button">
+export default function navButton(img, text, activeFlag = false){
+    let html = `<div class="rectangle-button ${checkActive(activeFlag)}">
                     <img src="${img}" alt="">
                     <h4>${text}</h4>
                 </div>`;
     return html;
+}
+
+function checkActive(flag){
+    if(flag)
+        return 'active'
+    else
+        return ''
 }
