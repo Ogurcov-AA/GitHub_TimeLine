7b89d66d7462e86819a534944ee85499cf36b4b0
Sasha Ogurtsov <sanekLLIypa@yandex.ru>
2021-07-24 16:38:23 +0300

    feat: add pagination, add filter for pagination and per_page

--- src/js/pages/repos.js
+++ src/js/pages/repos.js
@@ -1,93 +1,112 @@
 import leftMenu from '../component/leftMenu'
 import reposCard from "../component/reposCard";
 import store from "../store";
+import {pagination, createPaginationComp} from "../component/pagination";
 
 export default async function getHtml() {
-    settings()
     let html = `${leftMenu(parseURLGetName())}
                      <div class="main-field">
-                     <h4 class="textLabel">Public Repositories</h4>
-                     <div id="reposCard" class="reposGrid">
-                     <div class="loader"></div>
-                     </div>
-                     <div class="navigation">
-                     <div class="per_page">
-                     <span>per_page</span>
-                    <select name="per_page">
-                        <option value="6" selected>6</option>
-                        <option value="15">15</option>
-                        <option value="30">30</option>
-                    </select>
-                    
-                    </div>
-                    <div class="pagination">
-                        <!--pagination-->
-                    </div>
-                    </div>
+                        <h4 class="textLabel">Public Repositories</h4>
+                            <div id="reposCard" class="reposGrid">
+                                <div class="loader"></div>
+                            </div>
+                        <div class="navigation">
+                          <div class="navigation-wrapper">
+                             <div class="per_page">
+                                <span>per_page</span>
+                        <select name="per_page">
+                            <option value="6" selected>6</option>
+                            <option value="15">15</option>
+                            <option value="30">30</option>
+                        </select>
+                            </div>
+                           <div class="pagination">
+                                ${pagination()}
+                           </div>
+                          </div>
+                        </div>
                      </div>`;
-    createGrid()
+
     return html;
 }
 
-let reposCount = 0;
+let per_page = Number(localStorage.getItem('per_page')) || 6;
+let page = Number(localStorage.getItem('currentPage')) || 1;
 let reposList = [];
 
-function settings() {
-
-    getReposCount()
-}
-
-if(location.pathname.substring(location.pathname.lastIndexOf('/')+1)==='repos'){
-    window.onload = () =>{
+if (location.pathname.substring(location.pathname.lastIndexOf('/') + 1) === 'repos') {
+    window.onload = () => {
         let select = document.getElementsByName('per_page')[0]
-        select.onchange = () => {
-            createGrid(select.options[select.selectedIndex].value)
+        for(let i=0;i<select.options.length;i++){
+                if(Number(select.options[i].value)===per_page){
+                    select.options[i].selected = true
+                }
         }
-        createGrid(select.options[select.selectedIndex].value)
+        select.onchange = () => {
+            per_page = select.options[select.selectedIndex].value
+            setFilter(Number(per_page),1)
+            page=1
+            createGrid(per_page)
+            createPagination(page)
+        }
+        createGrid(per_page,page)
+        createPagination(page)
     }
-
 }
 
-async function createGrid(size,page=1) {
-let elem = document.getElementById('reposCard')
-    while(elem?.firstChild){
+async function createGrid(size, page = 1) {
+    let elem = document.getElementById('reposCard')
+    while (elem?.firstChild) {
         elem.removeChild(elem?.firstChild);
     }
-    let res = await getReposList(size,page)
-     for(let i =0;i<res.length;i++){
+    let res = await getReposList(size, page)
+    for (let i = 0; i < res.length; i++) {
         elem?.insertAdjacentHTML('afterbegin',
-            `${reposCard(res[i].name,'', res[i].language, res[i].htmlUrl, 'size' + size)}`)
-         elem.onclick = () => {
+            `${reposCard(res[i].name, '', res[i].language, res[i].htmlUrl, 'size' + size)}`)
+        elem.onclick = () => {
             location.pathname = `/repos/${store.userObject.getters.getName()}/${res[i].name}`
-         }
-
+        }
     }
 }
 
-async function getReposList(per_page,page) {
-
-    let res = await getRepos(parseURLGetName(),per_page,page)
+async function getReposList(per_page, page) {
+    let res = await getRepos(per_page, page)
     reposList = res
     return res
 }
 
-function parseURLGetName(){
-   return location.pathname.substring(location.pathname.indexOf('/',1)+1,location.pathname.lastIndexOf('/'))
+function parseURLGetName() {
+    return location.pathname.substring(location.pathname.indexOf('/', 1) + 1, location.pathname.lastIndexOf('/'))
 }
 
-async function getReposCount() {
-   let res = await getRepos();
-   reposCount = res.length
+async function createPagination(page) {
+    let count = store.userObject.getters.getReposCount()
+    createPaginationComp(Number(count), per_page, page, function (per_page, page) {
+        createGrid(per_page, page)
+        setFilter(null,Number(page))
+        createPagination(page)
+    })
 }
 
-function getRepos(per_page=null, page=null){
-    store.repository.actions.getRepositoryInfo(per_page,page)
-    let timer = new Promise(function (resolve,reject){
-    setInterval( () =>{
-        if (store.repository.getters.getLoading() !== true && store.repository.getters.checkLoadingRepos() !== true) {
-            resolve(store.repository.getters.getRepos())
-        }
-    }, 100)
+function getRepos(per_page = null, page = null) {
+    store.repository.actions.getRepositoryInfo(parseURLGetName(), per_page, page)
+    let timer = new Promise(function (resolve, reject) {
+        setInterval(() => {
+            if (store.repository.getters.getLoading() !== true && store.repository.getters.checkLoadingRepos() !== true) {
+                resolve(store.repository.getters.getRepos())
+            }
+        }, 100)
     })
     return timer
+}
+
+function setFilter(per_page=null,page=null){
+    if(page){
+        localStorage.removeItem('currentPage')
+        localStorage.setItem('currentPage',page)
+    }
+    if(per_page){
+        localStorage.removeItem('per_page')
+        localStorage.setItem('per_page',per_page)
+    }
 }
